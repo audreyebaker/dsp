e3b74abe7da12811efe9c36d62291a71f0870a01
audreyebaker <33380293+audreyebaker@users.noreply.github.com>
2018-01-06 20:37:25 -0500

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,36 @@
-# Hint:  use Google to find python function
+date_start = '01-02-2013'    
+date_stop = '07-28-2015'
 
-####a) 
-date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+from datetime import datetime
 
-####b)  
-date_start = '12312013'  
-date_stop = '05282015'  
+def days_between(d1, d2):
+     d1 = datetime.strptime(d1, '%m-%d-%Y')
+     d2 = datetime.strptime(d2, '%m-%d-%Y')
+     answer = abs((d2-d1).days)
+     print(str(answer)+ ' days')
+     return answer
+    
 
-####c)  
-date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+date_start2 = '12312013'  
+date_stop2 = '05282015'  
+
+def days_between2(d1, d2):
+     d1 = datetime.strptime(d1, '%m%d%Y')
+     d2 = datetime.strptime(d2, '%m%d%Y')
+     answer = (abs((d2-d1).days))
+     print(str(answer)+ ' days')
+     return answer
+ 
+date_start3 = '15-Jan-1994'      
+date_stop3 = '14-Jul-2015'  
+
+def days_between3(d1, d2):
+     d1 = datetime.strptime(d1, '%d-%b-%Y')
+     d2 = datetime.strptime(d2, '%d-%b-%Y')
+     answer = abs((d2-d1).days)
+     print(str(answer)+ ' days')
+     return answer
+
+days_between(date_start, date_stop)
+days_between2(date_start2, date_stop2)
+days_between3(date_start3, date_stop3)
